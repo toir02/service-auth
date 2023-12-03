7b9fea457d213122b8f12f1d7884906cc8b068b5
toir02 <wend8765@gmail.com>
2023-12-03 20:48:59 +0300

    added model user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=12, verbose_name='Номер телефона', unique=True)
+    invite_code = models.CharField(max_length=6, verbose_name='Код приглашения', unique=True, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
