bae3e06057362b0be45aca6ca165640ec7f424a1
toir02 <wend8765@gmail.com>
2023-12-04 00:11:39 +0300

    added serializer method field with invited users

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from users.models import User
 from users.validators import PhoneNumberValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
+    invited_users = SerializerMethodField()
 
     class Meta:
         model = User
@@ -12,3 +14,9 @@ class UserSerializer(serializers.ModelSerializer):
         validators = [
             PhoneNumberValidator(field='phone_number')
         ]
+
+    def get_invited_users(self, obj):
+        if obj.invite_code:
+            invited_users = User.objects.filter(invite_code=obj.invite_code)
+            return UserSerializer(invited_users, many=True).data
+        return []
