edcea2f5225478a22810ccc5681b97f0e1e91588
toir02 <wend8765@gmail.com>
2023-12-03 23:32:02 +0300

    added refresh and access token views

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from users.apps import UsersConfig
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    ...
+    path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
