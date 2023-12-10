7c0f1b1fe3009b9e79ec512969670a3496e7ca0e
toir02 <wend8765@gmail.com>
2023-12-10 20:43:46 +0300

    added create invite code function

--- users/services.py
+++ users/services.py
@@ -4,3 +4,8 @@ import string
 
 def create_verification_code():
     return ''.join(random.choices(string.digits, k=4))
+
+
+def create_invite_code():
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choices(characters, k=6))
