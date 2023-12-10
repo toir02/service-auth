2f4449b01b86eb6b16b3f90c0376e800dd7365cd
toir02 <wend8765@gmail.com>
2023-12-10 20:43:53 +0300

    refactor serializer

--- users/serializers.py
+++ users/serializers.py
@@ -5,18 +5,11 @@ from users.models import User
 from users.validators import PhoneNumberValidator
 
 
-class UserSerializer(serializers.ModelSerializer):
-    invited_users = SerializerMethodField()
+class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('id', 'username', 'password', 'phone_number', 'is_active', 'verification_code')
         validators = [
             PhoneNumberValidator(field='phone_number')
         ]
-
-    def get_invited_users(self, obj):
-        if obj.invite_code:
-            invited_users = User.objects.filter(invite_code=obj.invite_code)
-            return UserSerializer(invited_users, many=True).data
-        return []
