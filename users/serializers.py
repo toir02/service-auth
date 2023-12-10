d8ddfa3cfff76e1a35f60a0739bb451adac9e974
toir02 <wend8765@gmail.com>
2023-12-10 20:46:58 +0300

    added profile serializer

--- users/serializers.py
+++ users/serializers.py
@@ -13,3 +13,17 @@ class RegisterSerializer(serializers.ModelSerializer):
         validators = [
             PhoneNumberValidator(field='phone_number')
         ]
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    invited_users = SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'phone_number', 'invited_users', 'activated_code', 'invite_code')
+
+    def get_invited_users(self, obj):
+        if obj.invite_code:
+            invited_users = User.objects.filter(invite_code=obj.invite_code)
+            return ProfileSerializer(invited_users, many=True).data
+        return []
