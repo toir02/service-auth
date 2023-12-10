e2247484d8a4eab121e41c08279643f2c5fd79d6
toir02 <wend8765@gmail.com>
2023-12-10 19:00:48 +0300

    added path to create user

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 from users.apps import UsersConfig
+from users.views import UserCreateAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+
+    path('create/', UserCreateAPIView.as_view(), name='create-user'),
 ]
