522f13438eaf00824706549a0c2cba2073053edf
toir02 <wend8765@gmail.com>
2023-12-14 18:11:00 +0300

    added activate invite code api view

--- users/views.py
+++ users/views.py
@@ -33,7 +33,7 @@ class UserCreateAPIView(generics.CreateAPIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class UserVerifyView(generics.UpdateAPIView):
+class UserVerifyAPIView(generics.UpdateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [IsUnauthenticated]
     queryset = User.objects.all()
@@ -56,3 +56,29 @@ class UserVerifyView(generics.UpdateAPIView):
 class UserRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = ProfileSerializer
     queryset = User.objects.all()
+
+
+class ActivateInviteCodeAPIView(generics.UpdateAPIView):
+    serializer_class = ProfileSerializer
+    queryset = User.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        user = self.get_object()
+        invite_code_to_activate = self.request.data.get("invite_code")
+
+        if not invite_code_to_activate:
+            return Response({'error': 'Invite code to activate is required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            instance = User.objects.get(invite_code=invite_code_to_activate)
+        except User.DoesNotExist:
+            return Response({'error': 'Invalid invite code.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if user.activated_invite_code:
+            return Response({'error': 'User has already activated an invite code.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user.activated_invite_code = invite_code_to_activate
+        user.save()
+
+        serializer = self.get_serializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
