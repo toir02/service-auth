d32456fd06a587bcf4d8b1c4e1dedecaa4f73456
toir02 <wend8765@gmail.com>
2023-12-14 18:00:45 +0300

    added path to activate invite code

--- users/urls.py
+++ users/urls.py
@@ -7,8 +7,9 @@ from rest_framework_simplejwt.views import (
 from users.apps import UsersConfig
 from users.views import (
     UserCreateAPIView,
-    UserVerifyView,
-    UserRetrieveAPIView
+    UserVerifyAPIView,
+    UserRetrieveAPIView,
+    ActivateInviteCodeAPIView
 )
 
 app_name = UsersConfig.name
@@ -18,6 +19,7 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 
     path('create/', UserCreateAPIView.as_view(), name='create-user'),
-    path('verify-user/<int:pk>/', UserVerifyView.as_view(), name='verify-user'),
+    path('verify-user/<int:pk>/', UserVerifyAPIView.as_view(), name='verify-user'),
     path('view/<int:pk>/', UserRetrieveAPIView.as_view(), name='retrieve-user'),
+    path('activate-invite-code/<int:pk>/', ActivateInviteCodeAPIView.as_view(), name='activate-invite-code'),
 ]
